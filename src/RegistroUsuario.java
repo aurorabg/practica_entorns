012f90e885800ac1443266f7b91ee488b5757507
Aurora <aurorabg2002@gmail.com>
2023-03-29 17:51:57 +0200

    Creo los getters y setters, creo más constructores y creo el método init

--- src/RegistroUsuario.java
+++ src/RegistroUsuario.java
@@ -1,7 +1,13 @@
 public class RegistroUsuario {
     public static void main(String[] args) {
+        RegistroUsuario r = new RegistroUsuario();
         ValidarCampos registro = new ValidarCampos("Carles_345", "carlescanals345@gmail.com", "Prova_99",
                 "abcd1234");
+
+        r.init(registro);
+    }
+
+    public void init(ValidarCampos registro) {
         registro.validarCampos(registro);
     }
 }
