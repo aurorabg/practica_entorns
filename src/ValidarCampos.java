92e8849067bebd436e0fe66ccf9705d0a21189cb
Arnau <arnauperecapo@paucasesnovescifp.cat>
2023-03-29 13:28:23 +0200

    comentado la primera parte del codigo

--- src/ValidarCampos.java
+++ src/ValidarCampos.java
@@ -3,30 +3,49 @@ import java.util.Random;
 public class ValidarCampos {
     public static void validarCampos(String nombre, String email, String password, String codigo) {
 
+        // Declara un Array de Strings con algunos nombres de usuario existentes.
         String[] nombresExistentes = { "Juan_123", "Maria_456", "Pedro-789", "Sofia_123", "Luisa-456",
                 "Pablo_789", "Ana_123", "Carlos-456", "Lucia_789", "Marta_123" };
+        
+        // Comprueba si la longitud del nombre de usuario es mayor que 16 caracteres.
+        // Si se cumple esta condición, se imprime un mensaje de error y se finaliza el programa con la instrucción "return".
         if (nombre.length() > 16) {
             System.out.println("El nombre de usuario no cumple con las restricciones.");
             return;
         }
+        // Comprueba si el primer carácter del nombre de usuario es una letra mayúscula.
+        // Si esta condición no se cumple, se imprime un mensaje de error y se finaliza el programa con la instrucción "return".
         if (!Character.isUpperCase(nombre.charAt(0))) {
             System.out.println("El nombre de usuario no cumple con las restricciones.");
             return;
         }
+        // Usa una expresión regular para verificar si el nombre de usuario cumple con un patrón específico.
+        // El patrón requerido es que el nombre de usuario comience con una letra mayúscula seguida de una o
+        // más letras minúsculas, seguido de un guión o guión bajo y luego tres dígitos. Si esta condición no
+        // se cumple, se imprime un mensaje de error y se finaliza el programa con la instrucción "return".
         if (!nombre.matches("^[A-Za-z][a-z]*[-_][0-9]{3}$")) {
             System.out.println("El nombre de usuario no cumple con las restricciones.");
             return;
         }
+        // validación adicional que se realiza después de las verificaciones anteriores y busca si el nombre
+        // de usuario ingresado ya existe en una lista preexistente de nombres de usuario, "nombresExistentes".
         for (String nombreExistente : nombresExistentes) {
             if (nombre.equalsIgnoreCase(nombreExistente)) {
                 System.out.println("El nombre de usuario ya existe.");
                 return;
             }
         }
+        // comprueba si el correo electrónico contiene el carácter "@" que separa el nombre de usuario del dominio.
+        // Si no se cumple esta condición, se imprime un mensaje de error y se finaliza el programa con la instrucción "return".
         if (!email.contains("@")) {
             System.out.println("El correo electrónico no cumple con las restricciones.");
             return;
         }
+        // verifica si una dirección de correo electrónico cumple con ciertas restricciones.
+        // Utiliza una expresión regular para verificar si la dirección de correo electrónico termina en
+        //  “@paucasesnovescifp.com”, “@paucasesnovescifp.es”, “@paucasesnovescifp.cat”, “@yahoo.com”, “@yahoo.es”,
+        //  “@yahoo.cat”, “@gmail.com”, “@gmail.es”, “@gmail.cat”, “@hotmail.com”, “@hotmail.es” o “@hotmail.cat”.
+        // Si la dirección de correo electrónico no cumple con estas restricciones, se imprime un mensaje y se sale del método actual
         if (!email.matches("^.*@(paucasesnovescifp|yahoo|gmail|hotmail)\\.(com|es|cat)$")) {
             System.out.println("El correo electrónico no cumple con las restricciones.");
             return;
